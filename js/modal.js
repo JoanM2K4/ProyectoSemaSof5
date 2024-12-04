866cf8166b45e4a2c9ad1883dee4e19419eb34dd
JoanM2K4 <121960937+JoanM2K4@users.noreply.github.com>
2024-12-04 00:38:20 -0500

    Ventana de bienvenida modal

---
+++ js/modal.js
@@ -0,0 +1,14 @@
+let modalBitcoin = document.getElementById('modal');
+let closeModal = document.getElementById('close');
+
+// Cerrar Modal Btn
+
+closeModal.onclick = function(){
+    modalBitcoin.style.visibility = "hidden";
+}
+
+// Cerrar en ventana 
+
+modalBitcoin.onclick = function(){
+    modalBitcoin.style.visibility = "hidden";
+}
